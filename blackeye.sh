dc2a7766ba106e491d12e80a5e5fe8658fc553fa
Daniel Aaron <daniel.aaron@peratonlabs.com>
2023-10-27 13:24:09 -0400

    updated version

--- blackeye.sh
+++ blackeye.sh
@@ -211,13 +211,13 @@ fi
 
 banner() {
 
-printf "     \e[101m\e[1;77m::                    *BLACKEYE v2.0*.                       ::\e[0m\n"
+printf "     \e[101m\e[1;77m::                    *BLACKEYE v3.0*.                       ::\e[0m\n"
 printf "\n"
 printf "     \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
 printf "     \e[101m\e[1;77m:: responsible for any misuse or damage caused by BlackEye.  ::\e[0m\n"
 printf "     \e[101m\e[1;77m:: Only use for educational purporses!!                      ::\e[0m\n"
 printf "\n"
-printf "     \e[101m\e[1;77m::          Follow on Instagram @thebhikandeshmukh           ::\e[0m\n"
+printf "     \e[101m\e[1;77m::          https://github.com/majordmg			             ::\e[0m\n"
 printf "\n"
 }
 
